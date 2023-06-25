9bb7a7cc33a6a1143e33eac97d5c39f0f6ab4621
gabsilva <gabrielp.siilva@gmail.com>
2023-06-25 14:40:04 -0300

    adding error handler

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const routes = require('./routes');
+require('express-async-errors');
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use((error, request, response, next) => {
+  console.log('-------- Error Handler --------');
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('🔥 Server running on http://localhost:3000'));
